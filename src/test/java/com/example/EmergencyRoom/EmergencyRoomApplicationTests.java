e0106ada4c907f57ae247d3a0e3ed068fdba565a
Ernesto V R <ernesto.venzano.rodriguez@myy.haaga-helia.fi>
2020-03-31 13:42:42 +0300

    first commit: project created with Spring initalizer

---
+++ src/test/java/com/example/EmergencyRoom/EmergencyRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmergencyRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
